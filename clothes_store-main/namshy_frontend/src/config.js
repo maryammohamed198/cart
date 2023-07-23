4e450568f2086e7363a5d2acd52157573044578d
Mahmoud Eldeeb <116121838+Eldeeb71@users.noreply.github.com>
2023-07-23 16:30:35 +0300

    audio

--- clothes_store-main/namshy_frontend/src/config.js
+++ clothes_store-main/namshy_frontend/src/config.js
@@ -1,2 +1,2 @@
-export const backend_url = 'http://localhost:5000'
-// export const backend_url = 'https://wild-rose-gecko-cap.cyclic.app'
+//export const backend_url = 'http://localhost:5000'
+export const backend_url = 'https://wild-rose-gecko-cap.cyclic.app'
